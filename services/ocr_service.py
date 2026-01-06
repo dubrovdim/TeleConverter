9db8718098ea62c85a45ac6b4b3246fd60952966
dubrovdim <dubrovdmitriy28@gmail.com>
2026-01-06 23:22:45 +0200

    Change OCR language setting to auto detection

--- services/ocr_service.py
+++ services/ocr_service.py
@@ -16,7 +16,7 @@ class OCRService:
                     payload = {
                         'apikey': self.api_key,
                         'OCREngine': 2,
-                        'language': 'eng',
+                        'language': auto,
                         'scale': True,
                         'detectOrientation': True,
                         'isOverlayRequired': False
